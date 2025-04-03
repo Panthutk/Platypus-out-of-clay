4ca4fa331ad50a061e5fe7991de4c9e4b7213175
Panthut Ketphan <panthat.k@gmail.com>
2025-04-03 11:20:31 +0700

    check point before make enemy weapon

--- enemy.py
+++ enemy.py
@@ -9,25 +9,61 @@ ENEMY_TYPES = [
     {
         "name": "Fighter",
         "path": "Enemy/EnemyShipAsset/Base/Fighter.png",
+        "engine" : "Enemy/EnemyShipAsset/Engines/Fighter.png",
+        "weaponAnimation" : "Enemy/EnemyShipAsset/Weapons/Fighter.png",
+        "projectile" : "Enemy/EnemyShipAsset/Projectiles/Ray.png",
+        "destroyed" : "Enemy/EnemyShipAsset/Destroyed/Fighter.png",
         "speed": 3,
-        "size": (144, 144),
+        "size": (192,192),
+        "health": 20,
+        "projectile_speed": 5,
+        "score": 100,
     },
     {
         "name": "Torpedo",
         "path": "Enemy/EnemyShipAsset/Base/Torpedo.png",
+        "engine" : "Enemy/EnemyShipAsset/Engines/Torpedo.png",
+        "weaponAnimation" : "Enemy/EnemyShipAsset/Weapons/Torpedo.png",
+        "projectile" : "Enemy/EnemyShipAsset/Projectiles/Torpedo.png",
+        "destroyed" : "Enemy/EnemyShipAsset/Destroyed/Torpedo.png",
         "speed": 2.5,
-        "size": (144, 144),
+        "size": (192,192),
+        "health": 30,
+        "projectile_speed": 4,
+        "score": 150,
     },
     {
         "name": "Battlecruiser",
         "path": "Enemy/EnemyShipAsset/Base/Battlecruiser.png",
+        "engine" : "Enemy/EnemyShipAsset/Engines/Battlecruiser.png",
+        "weaponAnimation" : "Enemy/EnemyShipAsset/Weapons/Battlecruiser.png",
+        "projectile" : "Enemy/EnemyShipAsset/Projectiles/Rocket.png",
+        "destroyed" : "Enemy/EnemyShipAsset/Destroyed/Battlecruiser.png",
         "speed": 1.8,
-        "size": (192, 192),
+        "size": (240,240),
+        "health": 50,
+        "projectile_speed": 3,
+        "score": 200,
+    },
+]
+BOSSES = [
+    {
+        "name": "Dreadnought",
+        "path": "Enemy/EnemyShipAsset/Base/Dreadnought.png",
+        "engine" : "Enemy/EnemyShipAsset/Engines/Dreadnought.png",
+        "weaponAnimation" : "Enemy/EnemyShipAsset/Weapons/Dreadnought.png",
+        "projectile" : "Enemy/EnemyShipAsset/Projectiles/Ray.png",
+        "destroyed" : "Enemy/EnemyShipAsset/Destroyed/Dreadnought.png",
+        "speed": 1.0,
+        "size": (400,400),
+        "health": 200,
+        "projectile_speed": 2,
+        "score": 1000,
     },
 ]
 
 # Higher number = higher spawn chance
-SPAWN_WEIGHTS = [10, 4, 1]  # Fighter common, Torpedo , Battlecruiser
+SPAWN_WEIGHTS = [10, 4, 3]  # Fighter common, Torpedo , Battlecruiser
 
 
 class Enemy:
